55b31072b4580fa52a1857fcdd8cfe572378c8b6
Siddhart <Siddharth.pandey1@gmail.com>
2026-07-19 21:30:29 +0530

    floorandmodulus.py

---
+++ floordivisionandmodulus.py
@@ -0,0 +1,10 @@
+a= int(input("Enter the first number: "))
+b= int(input("Enter the second number: "))
+c= a//b
+
+d= int(input("Enter the third number: "))
+e= int(input("Enter the fourth number: "))
+f= d%e
+
+print("The floor division of", a, "and", b, "is:", c)
+print("The modulus of", d, "and", e, "is:", f)
